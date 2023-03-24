bdca52bf7de196363c48e3d0793a470035730c51
Finn McCoy <finnmccoy99@gmail.com>
2023-03-24 15:34:20 +0000

    Added test to ensure users cannot be saved without email or password

--- backend/spec/models/user.spec.ts
+++ backend/spec/models/user.spec.ts
@@ -16,6 +16,21 @@ describe('User model testing', () => {
     expect(newUser._id).toBeDefined();
   });
 
+  test('cannot save a user that does not have an email or password', async () => {
+    const noEmailUser = new User({
+      password: 'hello',
+    });
+    const noPasswordUser = new User({
+      email: 'test@yahoo.uk',
+    });
+    noEmailUser.save().catch((err) => {
+      expect(err).not.toBeNull();
+    });
+    noPasswordUser.save().catch((err) => {
+      expect(err).not.toBeNull();
+    });
+  });
+
   test('cannot save two users with identical emails', async () => {
     const user1 = new User({
       email: 'hello@example.com',
