d6d4c9f1fc0066cbc36fbf3496463c832136ca86
Finn McCoy <finnmccoy99@gmail.com>
2023-03-24 15:25:55 +0000

    Test drove ensuring that two users cannot have the same email

--- backend/spec/models/user.spec.ts
+++ backend/spec/models/user.spec.ts
@@ -5,7 +5,7 @@ describe('User model testing', () => {
     await User.deleteMany({});
   });
 
-  test('Can create and save new user document with email and password', async () => {
+  test('can create and save new user document with email and password', async () => {
     const newUser = new User({
       email: 'hello@example.com',
       password: 'password123',
@@ -14,5 +14,20 @@ describe('User model testing', () => {
     expect(newUser.password).toBe('password123');
     await newUser.save();
     expect(newUser._id).toBeDefined();
+  });
+
+  test('cannot save two users with identical emails', async () => {
+    const user1 = new User({
+      email: 'hello@example.com',
+      password: 'password123',
+    });
+    await user1.save();
+    const user2 = new User({
+      email: 'hello@example.com',
+      password: 'verysecure',
+    });
+    await user2.save().catch((err) => {
+      expect(err).not.toBeNull();
+    });
   });
 });
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -6,7 +6,7 @@ interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
