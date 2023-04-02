54457725b2df0cfb54b4b7d6b631d105baca4a4c
Finn McCoy <finnmccoy99@gmail.com>
2023-04-02 12:48:31 +0100

    Configured the app to read the JWT_SECRET environment variable

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import tokenRouter from './routes/tokenRouter';
+import dotenv from 'dotenv';
 const app: Express = express();
-
+dotenv.config();
 app.use(express.json());
 app.use('/tokens', tokenRouter);
 
