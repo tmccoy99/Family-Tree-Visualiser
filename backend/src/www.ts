c94d00974cb36b6e9ee1ad59b08d515c4467dccd
Finn McCoy <finnmccoy99@gmail.com>
2023-04-02 12:11:38 +0100

    Configured nodemon to compile and update server

--- backend/src/www.ts
+++ backend/src/www.ts
@@ -1,15 +1,22 @@
 import app from './app';
 import http, { Server } from 'http';
 
-const port = 8080;
+const port = 8085;
 app.set('port', port);
 
 const server: Server = http.createServer(app);
 server.listen(port);
 server.on('listening', onListening);
+process.on('SIGHUP', onShutdown);
 
 function onListening(): void {
   const addr = server.address() as any;
   const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   console.log('Now listening on ' + bind);
+}
+
+async function onShutdown(): Promise<void> {
+  console.log('Shutting down server');
+  await server.close();
+  process.exit(0);
 }
