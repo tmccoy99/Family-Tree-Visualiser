1b3765c4596a7e95c88f055f3b2e5e5c7b2b3878
Finn McCoy <finnmccoy99@gmail.com>
2023-04-03 09:05:04 +0100

    POST /users now creates new user in database

--- backend/spec/controllers/UserController.spec.ts
+++ backend/spec/controllers/UserController.spec.ts
@@ -1,8 +1,16 @@
 import '../mongodb_test_setup';
 import app from '../../src/app';
-import testRequest from 'supertest';
+import testRequest, { Test } from 'supertest';
+import User, { IUser } from '../../src/models/user';
 
 describe('UserController testing', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await User.deleteMany({});
+  });
   describe('POST /users route', () => {
     test('response to valid email and password has status 201 and message ok', async () => {
       const response = await testRequest(app)
@@ -10,6 +18,16 @@ describe('UserController testing', () => {
         .send({ email: 'hello@testing.com', password: 'testpassword' });
       expect(response.status).toBe(201);
       expect(response.body.message).toBe('OK');
+    });
+
+    test('if email and password valid, user will be saved to database', async () => {
+      await testRequest(app)
+        .post('/users')
+        .send({ email: 'hello@testing.com', password: 'testpassword' });
+      const savedUser: IUser | null = await User.findOne({
+        email: 'hello@testing.com',
+      });
+      expect(savedUser).not.toBeNull();
     });
   });
 });
--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
 interface IUserController {
-  Create: (req: Request, res: Response) => void;
+  Create: (req: Request, res: Response) => Promise<void>;
 }
 
 const UserController: IUserController = {
-  Create: function (req, res) {
+  Create: async function (req, res) {
+    const newUser = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser.save();
     res.status(201).json({ message: 'OK' });
   },
 };
