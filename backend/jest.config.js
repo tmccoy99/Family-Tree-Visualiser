7b1e86655c1188a78a42d5df4c31b0e347498795
Finn McCoy <finnmccoy99@gmail.com>
2023-04-03 10:38:45 +0100

    Removed global setup and teardown

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(spec))\\.ts$',
-  globalSetup: './spec/test_setup',
-  globalTeardown: './spec/test_teardown',
   maxWorkers: 1,
 };
