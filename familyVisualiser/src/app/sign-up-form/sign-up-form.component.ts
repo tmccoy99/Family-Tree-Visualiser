f610e004fce617436f3d823db235bcdb1f568683
Finn McCoy <finnmccoy99@gmail.com>
2023-04-12 11:31:25 +0100

    Tested and implemented submit sending request to POST /users

--- familyVisualiser/src/app/sign-up-form/sign-up-form.component.ts
+++ familyVisualiser/src/app/sign-up-form/sign-up-form.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-up-form',
   templateUrl: './sign-up-form.component.html',
-  styleUrls: ['./sign-up-form.component.css']
+  styleUrls: ['./sign-up-form.component.css'],
 })
 export class SignUpFormComponent {
-
+  requestNewUser(email: string, password: string): Promise<Response> {
+    return fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify({ email: email, password: password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
--- familyVisualiser/src/app/sign-up-form/sign-up-form.cy.ts
+++ familyVisualiser/src/app/sign-up-form/sign-up-form.cy.ts
@@ -2,26 +2,26 @@ import { Interception } from 'cypress/types/net-stubbing';
 import { SignUpFormComponent } from './sign-up-form.component';
 
 describe('Sign up form', () => {
-  beforeEach(() => {
-    cy.mount(SignUpFormComponent);
-  });
   test('it should have an email input and a password input', () => {
+    cy.mount(SignUpFormComponent);
     cy.get('input#signup-email-input');
     cy.get('input#signup-password-input');
   });
 
   test('it should have a submit button with text "Submit"', () => {
+    cy.mount(SignUpFormComponent);
     cy.get('button#submit-signup').should('have.text', 'Submit');
   });
 
   test('clicking submit button sends POST /users request with email and password', () => {
+    cy.mount(SignUpFormComponent);
     cy.get('input#signup-email-input').type('test@example.com');
     cy.get('input#signup-password-input').type('test');
     cy.intercept('POST', '/users').as('createUser');
     cy.get('button#submit-signup').click();
     cy.wait('@createUser').then((interception: Interception) => {
-      interception.request.body.email.should('equal', 'test@example.com');
-      interception.request.body.password.should('equal', 'test');
+      expect(interception.request.body.email).to.equal('test@example.com');
+      expect(interception.request.body.password).to.equal('test');
     });
   });
 });
