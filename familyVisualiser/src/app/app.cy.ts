8cfbd82ec51456b9ab3c937326b1b4aa3fb0b21c
Finn McCoy <finnmccoy99@gmail.com>
2023-04-12 09:39:58 +0100

    Tested header displaying correct text and image

--- familyVisualiser/src/app/app.cy.ts
+++ familyVisualiser/src/app/app.cy.ts
@@ -1,8 +1,19 @@
 import { AppComponent } from './app.component';
 
 describe('App component', () => {
-  test('App should render', () => {
+  beforeEach(() => {
     cy.mount(AppComponent);
+  });
+
+  test('app should render', () => {
     cy.get('main');
+  });
+
+  test('header with text TreeGenie and logo image should be rendered', () => {
+    const header = cy.get('#site-header');
+    header.should('contain.text', 'TreeGenie');
+    header.within(() => {
+      cy.get('img').should('have.attr', 'src', '../assets/logo.png');
+    });
   });
 });
