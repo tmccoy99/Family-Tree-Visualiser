a1f4c8571e86f983ad16a5d61a807e30127c0546
Finn McCoy <finnmccoy99@gmail.com>
2023-04-12 10:55:31 +0100

    Created sign-up-form component with basic tests

--- familyVisualiser/src/app/app.module.ts
+++ familyVisualiser/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignUpFormComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
