0e2f6f241c3a2aba177f8170ad096d2708154c66
Finn McCoy <finnmccoy99@gmail.com>
2023-04-14 12:01:31 +0100

    Added cors to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,14 @@ import tokenRouter from './routes/tokenRouter';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRouter';
 import JWT, { Secret } from 'jsonwebtoken';
+import logger from 'morgan';
+import cors from 'cors';
 
 const secret = process.env.JWT_SECRET as Secret;
 const app: Express = express();
 dotenv.config();
+app.use(logger('dev'));
+app.use(cors());
 
 app.use(express.json());
 app.use('/tokens', tokenRouter);
