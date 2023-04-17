6d0bd20dc29224cfac319ca3d5e388b7826daed2
Finn McCoy <finnmccoy99@gmail.com>
2023-04-17 11:53:06 +0100

    Tested and implemented handling of imageURL property

--- backend/spec/models/family-member.spec.ts
+++ backend/spec/models/family-member.spec.ts
@@ -12,4 +12,20 @@ describe('Family Member Model', () => {
     expect(newMember.name).toBe('Mr Test');
     expect(newMember.birthYear).toBe(1990);
   });
+
+  test('imageURL can be given, else default is assigned', async () => {
+    const memberWithPhoto = new FamilyMember({
+      name: 'Mr Test',
+      birthYear: 1990,
+      imageURL: 'example/url',
+    });
+    const memberWithoutPhoto = new FamilyMember({
+      name: 'Suzie',
+      birthYear: 1890,
+    });
+    await memberWithPhoto.save();
+    await memberWithoutPhoto.save();
+    expect(memberWithPhoto.imageURL).toBe('example/url');
+    expect(memberWithoutPhoto.imageURL).toBe('../../assets/default_photo.png');
+  });
 });
--- backend/src/models/family-member.ts
+++ backend/src/models/family-member.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, model, Document } from 'mongoose';
 interface IFamilyMember extends Document {
   name: string;
   birthYear: number;
+  imageURL: string;
 }
 
 const FamilyMemberSchema = new Schema<IFamilyMember>({
   name: { type: String, required: true },
   birthYear: { type: Number, required: true },
+  imageURL: { type: String, default: '../../assets/default_photo.png' },
 });
 
 export default model<IFamilyMember>('Family Member', FamilyMemberSchema);
