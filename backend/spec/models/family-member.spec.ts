a0a2c0e5ff21e31ea37323fb0a677f0b1c983784
Finn McCoy <finnmccoy99@gmail.com>
2023-04-17 12:20:40 +0100

    Tested and implemented optional schema attributes

--- backend/spec/models/family-member.spec.ts
+++ backend/spec/models/family-member.spec.ts
@@ -1,7 +1,12 @@
 import FamilyMember from '../../src/models/family-member';
 import '../mongodb_test_setup';
+import mongoose from 'mongoose';
 
 describe('Family Member Model', () => {
+  beforeEach(async () => {
+    await FamilyMember.deleteMany({});
+  });
+
   test('can create and save model with required attributes', async () => {
     const newMember = new FamilyMember({
       name: 'Mr Test',
@@ -27,5 +32,20 @@ describe('Family Member Model', () => {
     await memberWithoutPhoto.save();
     expect(memberWithPhoto.imageURL).toBe('example/url');
     expect(memberWithoutPhoto.imageURL).toBe('../../assets/default_photo.png');
+  });
+
+  test('non-required fields can also be given', async () => {
+    const newMember = new FamilyMember({
+      name: 'Mr Test',
+      birthYear: 1890,
+      deathYear: 1990,
+      children: [new mongoose.Types.ObjectId(), new mongoose.Types.ObjectId()],
+      spouse: new mongoose.Types.ObjectId(),
+    });
+    await newMember.save();
+    expect(newMember.id).toBeTruthy();
+    expect(newMember.deathYear).toBe(1990);
+    expect(newMember.children).toHaveLength(2);
+    expect(newMember.spouse).toBeTruthy();
   });
 });
--- backend/src/models/family-member.ts
+++ backend/src/models/family-member.ts
@@ -1,15 +1,21 @@
-import mongoose, { Schema, model, Document } from 'mongoose';
+import mongoose, { Schema, model, Document, mongo } from 'mongoose';
 
 interface IFamilyMember extends Document {
   name: string;
   birthYear: number;
   imageURL: string;
+  deathYear?: number;
+  children: mongoose.Types.ObjectId[];
+  spouse?: mongoose.Types.ObjectId;
 }
 
 const FamilyMemberSchema = new Schema<IFamilyMember>({
   name: { type: String, required: true },
   birthYear: { type: Number, required: true },
   imageURL: { type: String, default: '../../assets/default_photo.png' },
+  deathYear: Number,
+  children: [{ type: mongoose.Types.ObjectId, ref: 'Family Member' }],
+  spouse: { type: mongoose.Types.ObjectId, ref: 'Family Member' },
 });
 
 export default model<IFamilyMember>('Family Member', FamilyMemberSchema);
